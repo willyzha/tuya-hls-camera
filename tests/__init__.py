f1fcec1d20d7dc3daf9a071bc03812f4ba831767
Willy <willyzha@gmail.com>
2022-01-02 13:00:47 -0500

    first commit

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for Tuya HLS Camera integration."""
---
+++ tests/const.py
@@ -0,0 +1,9 @@
+"""Constants for Tuya HLS Camera tests."""
+from custom_components.tuya_hls_camera.const import (
+    CONF_PASSWORD,
+)
+from custom_components.tuya_hls_camera.const import (
+    CONF_USERNAME,
+)
+
+MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
